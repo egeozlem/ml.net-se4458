92b91dbf335e585921d71a01271f74c065b01e6f
egeozlem <ege.ozlem@hotmail.com>
2024-12-02 16:15:59 +0300

    first commit
    added the demo

---
+++ MLnetDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using MLnetDemo;
+Console.WriteLine("Write a comment: ");
+var sampleInput = Console.ReadLine();
+var input = new SentimentModel.ModelInput()
+{
+    Col0 = sampleInput
+};
+if (sampleInput == null)
+{
+    Console.WriteLine("please write something");
+}
+else
+   
+{
+    var result = SentimentModel.Predict(input);
+
+    var sentiment = result.PredictedLabel == 1 ? "positive comment" : "negative comment";
+    var maxConfidence = result.Score.Max();
+    Console.WriteLine($"Text: {input.Col0}\nGuess: {sentiment}");
+    Console.WriteLine($"Confidence: %{maxConfidence*100:F2}");
+    
+}
+
+
